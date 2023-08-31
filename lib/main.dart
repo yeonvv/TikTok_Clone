a673544c4ba59bcfd9b52573cf1a3059dcc91c53
yeonvv <yeonvv8@gmail.com>
2023-08-31 18:13:24 +0900

    setup

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: Container(
+        child: const Row(
+          children: [
+            Text(
+              "hello",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
