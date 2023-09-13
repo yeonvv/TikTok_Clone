679d614834a76f762f3aa3c1eb17041c73c6ba10
yeonvv <yeonvv8@gmail.com>
2023-09-13 19:57:29 +0900

    onDismissed

--- lib/features/indox/activity_screen.dart
+++ lib/features/indox/activity_screen.dart
@@ -2,10 +2,21 @@ import 'package:dart_tiktok/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class ActivityScreen extends StatelessWidget {
+class ActivityScreen extends StatefulWidget {
   const ActivityScreen({
     super.key,
   });
+
+  @override
+  State<ActivityScreen> createState() => _ActivityScreenState();
+}
+
+class _ActivityScreenState extends State<ActivityScreen> {
+  final List<String> _notifications = List.generate(8, (index) => "${index}h");
+  void _onDismissed(String notification) {
+    _notifications.remove(notification);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,57 +38,87 @@ class ActivityScreen extends StatelessWidget {
               ),
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 1,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Container(
-                    width: Sizes.size52,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Colors.black12,
+          for (var notification in _notifications)
+            Expanded(
+              child: ListView.builder(
+                itemCount: 1,
+                itemBuilder: (context, index) {
+                  return Dismissible(
+                    onDismissed: (direction) => _onDismissed(notification),
+                    key: Key(notification),
+                    background: Container(
+                      color: const Color(0xff61D4F0),
+                      alignment: Alignment.centerLeft,
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: Sizes.size20),
+                        child: FaIcon(
+                          FontAwesomeIcons.checkDouble,
+                          size: Sizes.size28,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                    child: const Center(
-                      child: FaIcon(
-                        FontAwesomeIcons.bell,
-                        color: Colors.black,
+                    secondaryBackground: Container(
+                      color: Theme.of(context).primaryColor,
+                      alignment: Alignment.centerRight,
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: Sizes.size20),
+                        child: FaIcon(
+                          FontAwesomeIcons.trashCan,
+                          size: Sizes.size28,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                  title: RichText(
-                    text: const TextSpan(
-                      text: "Account updates:",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w600,
-                        fontSize: Sizes.size16,
+                    child: ListTile(
+                      leading: Container(
+                        width: Sizes.size52,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.black12,
+                          ),
+                        ),
+                        child: const Center(
+                          child: FaIcon(
+                            FontAwesomeIcons.bell,
+                            color: Colors.black,
+                          ),
+                        ),
                       ),
-                      children: [
-                        TextSpan(
-                            text: " Upload longer",
-                            style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                            )),
-                        TextSpan(
-                            text: " 1h",
-                            style: TextStyle(
-                              color: Colors.black26,
-                              fontWeight: FontWeight.normal,
-                            )),
-                      ],
+                      title: RichText(
+                        text: TextSpan(
+                          text: "Account updates:",
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                            fontSize: Sizes.size16,
+                          ),
+                          children: [
+                            const TextSpan(
+                                text: " Upload longer",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                )),
+                            TextSpan(
+                              text: " $notification",
+                              style: const TextStyle(
+                                color: Colors.black26,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      trailing: const FaIcon(
+                        FontAwesomeIcons.chevronRight,
+                        size: Sizes.size16,
+                      ),
                     ),
-                  ),
-                  trailing: const FaIcon(
-                    FontAwesomeIcons.chevronRight,
-                    size: Sizes.size16,
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
         ],
       ),
     );
