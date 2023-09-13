cf2d2b419b95e326eb063bd06b9d7167616de440
yeonvv <yeonvv8@gmail.com>
2023-09-13 17:51:59 +0900

    invoked OffStage in ActivityScreen

--- lib/features/indox/activity_screen.dart
+++ lib/features/indox/activity_screen.dart
@@ -3,14 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ActivityScreen extends StatelessWidget {
-  const ActivityScreen({super.key});
+  const ActivityScreen({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("All activity"),
-      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
--- lib/features/indox/indox_screen.dart
+++ lib/features/indox/indox_screen.dart
@@ -4,16 +4,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class InboxScreen extends StatelessWidget {
+class InboxScreen extends StatefulWidget {
   const InboxScreen({super.key});
 
-  void _onActivityTab(BuildContext context) {
-    Navigator.of(context).push(
-      PageRouteBuilder(
-        pageBuilder: (context, animation, secondaryAnimation) =>
-            const ActivityScreen(),
-      ),
-    );
+  @override
+  State<InboxScreen> createState() => _InboxScreenState();
+}
+
+class _InboxScreenState extends State<InboxScreen> {
+  bool _selectedActivity = false;
+
+  void _onActivityTab() {
+    _selectedActivity = !_selectedActivity;
+    setState(() {});
   }
 
   void _onDmPressed() {}
@@ -23,101 +26,116 @@ class InboxScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         elevation: 0.01,
-        title: const Text("Inbox"),
+        title: _selectedActivity
+            ? const Text("All activity")
+            : const Text("Inbox"),
         actions: [
           IconButton(
-            onPressed: _onDmPressed,
+            onPressed: _selectedActivity ? _onActivityTab : _onDmPressed,
             icon: const FaIcon(
               FontAwesomeIcons.paperPlane,
             ),
           ),
         ],
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(Sizes.size52),
-          child: Column(
-            children: [
-              const Divider(
-                height: Sizes.size16,
-                color: Colors.black12,
-              ),
-              ListTile(
-                onTap: () => _onActivityTab(context),
-                title: const Text(
-                  "Activity",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                trailing: const FaIcon(
-                  FontAwesomeIcons.chevronRight,
-                  size: Sizes.size16,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: Sizes.size10,
-              horizontal: Sizes.size20,
-            ),
-            child: Text(
-              "Messages",
-              style: TextStyle(
-                fontSize: Sizes.size16,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return Column(
+        bottom: _selectedActivity
+            ? null
+            : PreferredSize(
+                preferredSize: const Size.fromHeight(Sizes.size72),
+                child: Column(
                   children: [
+                    const Divider(
+                      height: Sizes.size16,
+                      color: Colors.black12,
+                    ),
                     ListTile(
-                      leading: Container(
-                        width: Sizes.size52,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.blue,
-                        ),
-                        child: const Center(
-                          child: FaIcon(
-                            FontAwesomeIcons.users,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
+                      onTap: _onActivityTab,
                       title: const Text(
-                        "New Followers",
+                        "Activity",
                         style: TextStyle(
                           fontWeight: FontWeight.w600,
                         ),
-                      ),
-                      subtitle: const Text(
-                        "Messages from followers will appear here",
                       ),
                       trailing: const FaIcon(
                         FontAwesomeIcons.chevronRight,
                         size: Sizes.size16,
                       ),
                     ),
-                    const Divider(
-                      height: 1,
-                      color: Colors.black38,
-                      endIndent: 0,
-                      indent: Sizes.size80,
-                    )
                   ],
-                );
-              },
+                ),
+              ),
+      ),
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedActivity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: Sizes.size10,
+                    horizontal: Sizes.size20,
+                  ),
+                  child: Text(
+                    "Messages",
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 15,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          ListTile(
+                            leading: Container(
+                              width: Sizes.size52,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.blue,
+                              ),
+                              child: const Center(
+                                child: FaIcon(
+                                  FontAwesomeIcons.users,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            title: const Text(
+                              "New Followers",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            subtitle: const Text(
+                              "Messages from followers will appear here",
+                            ),
+                            trailing: const FaIcon(
+                              FontAwesomeIcons.chevronRight,
+                              size: Sizes.size16,
+                            ),
+                          ),
+                          const Divider(
+                            height: 1,
+                            color: Colors.black38,
+                            endIndent: 0,
+                            indent: Sizes.size80,
+                          )
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
+          Offstage(
+            offstage: !_selectedActivity,
+            child: const ActivityScreen(),
+          )
         ],
       ),
     );
